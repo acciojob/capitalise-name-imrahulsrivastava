1efca91a5b7f8b88635bb5623bced6a3fbe7b97b
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-12-20 19:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+// Write your script here
+const input = document.getElementById("fname");
+input.addEventListener("change", function () {
+  input.value = input.value.toUpperCase();
+});
